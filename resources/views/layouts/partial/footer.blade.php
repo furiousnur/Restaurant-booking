1d0b3c97cea2595d150a3f56cf589c198a983e5c
Nur Alam <furiousnur@gmail.com>
2019-07-17 21:16:56 +0600

    done

---
+++ resources/views/layouts/partial/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <p class="copyright pull-right">
+            &copy;
+            <script>
+                document.write(new Date().getFullYear())
+            </script>
+            Design by <a href="http://www.dream360it.com">Dream360IT</a>, and Developed by <a href="https://github.com/furiousnur">Md Nur Alam</a>
+        </p>
+    </div>
+</footer>
